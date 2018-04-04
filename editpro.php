09e3bf005652d7ca360b5ead1ec33c41eb266b8a
Jocelyn100077826 <jocelynngshienlin@gmail.com>
2018-04-04 18:12:25 +0800

    Fix Navigation and Home page

--- editpro.php
+++ editpro.php
@@ -19,8 +19,8 @@
     
     <div class="container">
         
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid p-3 mb-2 bg-dark text-dark">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -35,20 +35,17 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Food Category</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Customer Profile <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="editpro.php">Edit Profile</a></li>
-            <li><a href="#">Log out</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
+			<li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Sets</a></li>
+				<li><a href="#">Categories</a></li>
+			  </ul>
+			</li>
+			<li><a href="editpro.php">My Account</a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></li>
         </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require 'server.php'; session_start();?>
+<!--<-?php require 'server.php'; session_start();?>-->
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,14 +10,12 @@
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5
     elements and media queries -->
-
-   
+   <link href="styles/style.css" rel="stylesheet" />
 </head>
 
 <body>
-    
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid p-3 mb-2 bg-dark text-dark">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -32,31 +30,100 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Food Category</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Customer Profile <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="editpro.php">Edit Profile</a></li>
-            <li><a href="#">Log out</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
+			<li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Sets</a></li>
+				<li><a href="#">Categories</a></li>
+			  </ul>
+			</li>
+			<li><a href="editpro.php">My Account</a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></li>
         </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-    
-    
 
+<!--START OF CAROUSEL-->
+
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="images/food3.jpg" alt="first">
+		<div class="carousel-caption">
+			<h2>Meat Lovers' Special</h2>
+			<p>Check out this month's special deal!</p>
+			</br>
+			</br>
+			</br>
+      </div>
+      </div>
+
+      <div class="item">
+        <img src="images/foody.jpg" alt="second" >
+		<div class="carousel-caption">
+			<h2>Kids Meals</h2>
+			<p>Coming soon!</p>
+			</br>
+			</br>
+			</br>
+      </div>
+      </div>
     
-    
-    
-    
+      <div class="item">
+        <img src="images/food1.jpg" alt="third">
+		<div class="carousel-caption">
+			<h2>Vegetarian Options</h2>
+			<p>Come try our natural selections!</p>
+			</br>
+			</br>
+			</br>
+      </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+<!--END OF CAROUSEL-->
+
+<div class="container">
+<div class="row">
+	<div class="section1">
+		<div class="col-xs-6"><span class="glyphicon glyphicon-leaf" aria-hidden="true"></span><h1>Natural</h1><p>Existing in or derived from nature; not made or caused by humankind.In accordance with the nature of, or circumstances surrounding, someone or something.</p></div>
+	</div>
+	<div class="section2">
+		<div class="col-xs-6"><span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span><h1>Quality</h1><p>The standard of something as measured against other things of a similar kind; the degree of excellence of something.</p></div>
+	</div>
+</div>
+</div>
+
+ <div class="footer">
+	<h3>Contact Information</h3>
+	<p>Steven : 010-8328234</p>
+	<p>Alberto : 019-43942934</p>
+	<p>Malibu : 013-24567892</p>
+</div> 
+
     
     <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
     <script src="bootstrap/js/jquery.min.js"></script>
