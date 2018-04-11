ac32c76b676e4b5c44bed2447698c17a2ca790c5
khaled2095 <kool_4_blitz@hotmail.com>
2018-04-11 20:46:16 +0800

    cart functionalities and selection/deselection of food

--- cart.php
+++ cart.php
@@ -1,3 +1,94 @@
+<?php
+session_start();
+
+$product_ids = array();
+
+/*session_destroy();*/
+/* check if add to cart button has been submitted */
+if(filter_input(INPUT_POST, 'addtocart')){
+    if(isset($_SESSION['shopping_cart'])){
+        
+        /* Keep track how many products are in the shopping cart*/
+        $count = count($_SESSION['shopping_cart']);
+        
+        /* create sequential array for matching array keys to product ids */
+        $product_ids = array_column($_SESSION['shopping_cart'], 'id');
+        
+        if(!in_array(filter_input(INPUT_GET, 'id'), $product_ids))
+        {
+        
+            $_SESSION['shopping_cart'][$count] = array
+            (
+                'id' => filter_input(INPUT_GET, 'id'),
+                'name' => filter_input(INPUT_POST, 'name'),
+                'price' => filter_input(INPUT_POST, 'price'),
+                'quantity' => filter_input(INPUT_POST, 'quantity')
+            );
+            
+        }
+        
+        else
+        {
+            /* product already exists , increase quantity  */
+            /* math array key to id of the product being added to the cart  */
+            for($i=0; $i < count($product_ids); $i++)
+            {
+                if($product_ids[$i] == filter_input(INPUT_GET, 'id'))
+                {
+                    /* add item quantity to the existing product in the array */
+                    
+                    $_SESSION['shopping_cart'][$i]['quantity'] += filter_input(INPUT_POST, 'quantity');
+                }
+            }
+        }
+        
+        
+    }
+    else /* if shopping cart does not exist, create first product with array key 0 */
+    {
+        /* create array using submitted form data, start from key 0 and fill it with values  */
+        $_SESSION['shopping_cart'][0] = array
+        (
+            'id' => filter_input(INPUT_GET, 'id'),
+            'name' => filter_input(INPUT_POST, 'name'),
+            'price' => filter_input(INPUT_POST, 'price'),
+            'quantity' => filter_input(INPUT_POST, 'quantity')
+        );
+    }
+}
+
+/* code for remove button */
+
+if(filter_input(INPUT_GET, 'action') == 'delete')
+{
+    /* loop through all the food items in the cart until it matches with GET id Variable */
+    foreach($_SESSION['shopping_cart'] as $key => $product)
+    {
+        if($product['id'] == filter_input(INPUT_GET, 'id'))
+        {
+            /* remove product from the shopping cart when it matches with the GET id */
+            unset($_SESSION['shopping_cart'][$key]);
+        }
+    }
+    
+    /* reset session array keys so they match with $product_ids numeric array */
+    $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']);
+}
+
+/* this will show how is the array working for storing food products */
+/*pre_r($_SESSION);*/
+
+function pre_r($array)
+{
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
